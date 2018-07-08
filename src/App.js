b6683199b7bced6a34b4691b254a08627d62b5db
ancisse <a.cisse@onmap-visual.com>
2018-07-08 08:06:59 +0200

    Post && search data using reactjs and elasticsearch

--- src/App.js
+++ src/App.js
@@ -1,68 +1,148 @@
-import React, { Component } from 'react';
+import React from 'react';
 import elasticsearch from "elasticsearch";
 import logo from './logo.svg';
 import './App.css';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+
 let client = new elasticsearch.Client({
   host: "localhost:9200",
   log: "trace",
   requestTimeout: Infinity,
-  keepAlive: true // Tested
+  keepAlive: true 
 
 });
-class App extends Component {
-	/*handleSubmit(event) {
-    alert('A name was submitted: ' + navigator.userAgent);
-    event.preventDefault();
-  }*/
+let indexName = 'test';
+class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = { results: [] };
   };
-  handleChange(event) {
-    const search_query = event.target.value;
-
-    client
-      .search({
-        q: search_query
-      })
-      .then(
-        function(body) {
-          this.setState({ results: body.hits.hits });
-        }.bind(this),
-        function(error) {
-          console.trace(error.message);
-        }
-      );
-  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
 		<img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React example using elasticsearch.js </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This example show :
+        </p>
+		<p className="App-intro">
+		  How to create document into elasticsearch backend  using Reactjs.
+        </p>
+		<p className="App-intro">
+		  How to retrive datas from elastic search and  display them  in data table.
         </p>
 		<SubmitForm></SubmitForm>
-		 
+		<SearchForm></SearchForm>
       </div>
     );
   };
 }
 
-class SubmitForm extends Component {
+
+class SearchForm extends React.Component {
+	constructor(props) {
+      super(props);
+      this.state = {
+         searchText: '',
+	     results: [] 
+      }
+      this.updateState = this.updateState.bind(this);
+      this.handleSearch = this.handleSearch.bind(this);
+   };
+   updateState(e) {
+      this.setState({searchText: e.target.value});
+   }
+	handleSearch(event) {
+	 const search_query = this.state.searchText;
+	 return client.search({
+		index: indexName,
+		q: 'user_agent:' + search_query
+	 }).then(function(body){
+		 console.log(body)
+		 this.setState({ results: body.hits.hits });
+	 }.bind(this),function(error){
+		  console.log(error)
+	 });
+
+    event.preventDefault();
+  }
+  render() {
+	  const  data  = this.state.results;
+    return (
+     <div>
+            <input value = {this.state.data} onChange = {this.updateState} 
+               ref = "myInput"></input>
+            <button onClick = {this.handleSearch}>Search</button>
+			<hr />
+			<div>
+			<ReactTable
+				  data={data}
+				  columns={[
+					{
+					  Header: "Infos",
+					  columns: [
+						{
+						  Header: "Post date",
+						  accessor: "_source.published_at"
+						},
+						{
+						  Header: "Browser infos",
+						  id: "_source.user_agent",
+						  accessor: d => d._source.user_agent
+						}
+					  ]
+					}
+				  ]}
+				  defaultPageSize={10}
+				  className="-striped -highlight"
+			/>
+		  </div>
+         </div>
+
+    );
+  };
+}
+class SubmitForm extends React.Component {
+     
 	handleSubmit(event) {
-    //alert('A name was submitted: ' + navigator.userAgent);
-	client.ping({
-		}, function (error) {
-		  if (error) {
-			console.error('elasticsearch cluster is down!');
-		} else {
-			console.log('All is well');
-		}
-	});
+		const existsIndexPromise =  client.indices.exists({
+			index: indexName
+		});
+		existsIndexPromise.then(function(exists){
+			if(!exists){
+				const initIndexPromise = client.indices.create({
+					index: indexName
+					});
+				initIndexPromise.then(function(){
+					return  client.indices.putMapping({
+						index: indexName,
+						type: 'userAgent',
+						body: {
+							properties: {
+								user_agent: { type: 'text' },
+								published_at: {type: 'date'}
+							}
+						}
+					});
+							
+				});
+				
+			}
+		}).then(function(){
+			const postPromise = client.index({
+							index:indexName,
+							type: 'userAgent',
+							body: {
+								user_agent: navigator.userAgent,
+								published_at: new Date()
+							}
+						});
+			postPromise.then(function(results){
+							console.log('Post  data  ', results);
+				})
+		})
     event.preventDefault();
   }
   render() {
